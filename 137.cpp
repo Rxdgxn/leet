084fd407fd357dc2bb7e89f0fb833439d67ffeb5
Rxdgxn <radugantoi@gmail.com>
2023-04-02 13:02:06 +0300

    Added 137

---
+++ 137.cpp
@@ -0,0 +1,14 @@
+#include "include.h"
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> m;
+
+        for (int num : nums) m[num]++;
+        for (int num : nums)
+            if (m[num] == 1) return num;
+
+        return -1;
+    }
+};
