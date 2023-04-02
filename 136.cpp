f052eb524459c0e10dbece294c4c32bef5e9ff8e
Rxdgxn <radugantoi@gmail.com>
2023-04-02 12:57:15 +0300

    Added 136

---
+++ 136.cpp
@@ -0,0 +1,14 @@
+#include "include.h"
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int occ[60001] = {0};
+
+        for (int num : nums) occ[30000 + num]++;
+        for (int num : nums)
+            if (occ[30000 + num] == 1) return num;
+
+        return -1;
+    }
+};
