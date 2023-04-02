f5ea59b760465dd4f89a89410c329e9a5eed5334
Rxdgxn <radugantoi@gmail.com>
2023-04-02 10:27:26 +0300

    Added reduced-string

---
+++ reduced-string.cpp
@@ -0,0 +1,25 @@
+#include "include.h"
+
+string superReducedString(string& s) {
+    int i = 0;
+
+    while (i < s.size() - 1 && s.size()) {
+        if (s[i] == s[i+1]) {
+            s.erase(s.begin() + i);
+            s.erase(s.begin() + i);
+            if (i) i--;
+        }
+        else i++;
+    }
+
+    return (s.size() ? s : "Empty String");
+}
+
+int main(void) {
+
+    string s;
+    cin >> s;
+    cout << superReducedString(s);
+
+    return 0;
+}
