22e8b9eae55ce1cb62314b9cf80d755688c25d5f
Rxdgxn <radugantoi@gmail.com>
2023-04-03 12:43:46 +0300

    Added 881

---
+++ 881.cpp
@@ -0,0 +1,21 @@
+#include "include.h"
+
+class Solution {
+public:
+    #define sort_v(v) sort((v).begin(), (v).end())
+
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort_v(people);
+        int i = 0, j = people.size() - 1;
+        int boats = 0;
+
+        while (i <= j) {
+            boats++;
+            if (people[i] + people[j] <= limit)
+                i++;
+            j--;
+        }
+
+        return boats;
+    }
+};
