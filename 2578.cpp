df5bd25116ee68cc5bb76c313dd90d0cede8b3d1
Rxdgxn <radugantoi@gmail.com>
2023-06-11 18:30:46 +0300

    Added 2578

---
+++ 2578.cpp
@@ -0,0 +1,24 @@
+#include "include.h"
+
+class Solution {
+public:
+    int splitNum(int num) {
+        vector<int> v;
+        while (num) {
+            v.push_back(num % 10);
+            num /= 10;
+        }
+        sort(v.begin(), v.end());
+
+        int n1 = 0, n2 = 0, sz = v.size();
+        for (int i = 0; i < sz - 1 - (sz % 2); i += 2) {
+            n1 = n1 * 10 + v[i];
+            n2 = n2 * 10 + v[i + 1];
+        }
+        if (sz % 2) {
+            n1 = n1 * 10 + v[sz - 1];
+        }
+
+        return n1 + n2;
+    }
+};
