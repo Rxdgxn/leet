b15e5124da5bbfb4e810e9cb1ad7e20946136503
Rxdgxn <radugantoi@gmail.com>
2023-07-10 19:29:09 +0300

    Added 2424

---
+++ src/2424.cpp
@@ -0,0 +1,24 @@
+#include "../include.h"
+
+class LUPrefix {
+public:
+
+    int mp[1001] = {0}, len, pre = 0;
+
+    LUPrefix(int n) {
+        len = n;
+    }
+
+    void upload(int video) {
+        mp[video] = 1;
+    }
+
+    int longest() {
+        for (int i = pre + 1; i <= len; i++) {
+            if (mp[i]) pre++;
+            else break;
+        }
+
+        return pre;
+    }
+};
