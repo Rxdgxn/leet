d21b65da2d1f9a1017b02b957b93edfd7cb04d7d
Rxdgxn <radugantoi@gmail.com>
2023-09-10 16:43:58 +0300

    Added 38

---
+++ src/38.cpp
@@ -0,0 +1,33 @@
+#include "../include.h"
+
+class Solution {
+public:
+    string func(int k, int n, string prev) {
+        char c = prev[0];
+        int cnt = 1;
+        string curr = "";
+
+        const int sz = prev.size();
+        for (int i = 1; i < sz; i++) {
+            if (prev[i] == c) cnt++;
+            else {
+                curr += to_string(cnt);
+                cnt = 1;
+                curr += c;
+                c = prev[i];
+            }
+        }
+
+        curr += to_string(cnt);
+        curr += prev[sz - 1];
+
+        if (k < n) return func(k + 1, n, curr);
+        return curr;
+    }
+
+    string countAndSay(int n) {
+        if (n == 1) return "1";
+        if (n == 2) return "11";
+        return func(3, n, "11");
+    }
+};
