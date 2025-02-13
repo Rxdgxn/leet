02978832fd9babadaa692b6dbdbe6a6c7a8ecf28
Rxdgxn <radugantoi@gmail.com>
2025-02-13 23:32:38 +0200

    Added 3066

---
+++ src/3066.cpp
@@ -0,0 +1,23 @@
+#include "../include.h"
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        priority_queue<long long, vector<long long>, greater<long long>> pq(nums.begin(), nums.end());
+
+        int ops = 0;
+
+        while (pq.top() < k) {
+            long long x = pq.top();
+            pq.pop();
+            long long y = pq.top();
+            pq.pop();
+
+            pq.push(min(x, y) * 2 + max(x, y));
+
+            ops++;
+        }
+
+        return ops;
+    }
+};
