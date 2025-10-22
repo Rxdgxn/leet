baad80b1fe943d95514dc775948a9b68b8ad646a
Rxdgxn <radugantoi@gmail.com>
2025-10-22 22:02:11 +0300

    Added 2249

---
+++ src/2249.cpp
@@ -0,0 +1,31 @@
+#include "../include.h"
+
+class Solution {
+public:
+    inline int distance(int x1, int y1, int x2, int y2) {
+        return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+    }
+
+    int countLatticePoints(vector<vector<int>>& circles) {
+        set<pair<int, int>> points;
+        // numeri pt un cadran, aduni pt fiecare punct alte 3 puncte
+
+        for (auto& c : circles) {
+            const int r = c[2];
+            const int cx = c[0], cy = c[1];
+
+            for (int i = 0; i <= r; i++) {
+                for (int j = 0; j <= r; j++) {
+                    if (distance(cx, cy, cx + i, cy + j) <= r * r) {
+                        points.insert({cx + i, cy + j});
+                        points.insert({cx - i, cy + j});
+                        points.insert({cx + i, cy - j});
+                        points.insert({cx - i, cy - j});
+                    }
+                }
+            }
+        }
+
+        return points.size();
+    }
+};
