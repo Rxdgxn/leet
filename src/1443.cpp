61467e2d8755462e5d9e6adde9f6e013e65150cc
Rxdgxn <radugantoi@gmail.com>
2026-04-27 23:46:57 +0300

    Added 1443

---
+++ src/1443.cpp
@@ -0,0 +1,38 @@
+#include "../include.h"
+
+class Solution {
+public:
+    unordered_map<int, vector<int>> graph;
+    int visit_time = 0;
+
+    int dfs(int node, int parent, const vector<bool>& hasApple) {
+        visit_time++;
+        int apples = hasApple[node];
+
+        for (int child : graph[node]) {
+            if (child == parent)
+                continue;
+
+            int child_apples = dfs(child, node, hasApple);
+            apples += child_apples;
+
+            if (child_apples > 0)
+                visit_time++;
+            else
+                visit_time--;
+        }
+
+        return apples;
+    }
+
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        for (auto& e : edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+
+        dfs(0, -1, hasApple);
+
+        return visit_time - 1;
+    }
+};
